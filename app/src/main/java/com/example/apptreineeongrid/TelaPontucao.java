210098e9201f6c793b32abb7318cbae18f60ac8a
Lipao12 <f.nunesmai19@gmail.com>
2021-04-12 21:01:54 -0300

    1

--- app/src/main/java/com/example/apptreineeongrid/TelaPontucao.java
+++ app/src/main/java/com/example/apptreineeongrid/TelaPontucao.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -30,8 +31,10 @@ public class TelaPontucao extends AppCompatActivity {
         this.mViewHolder.t_scoreFacil = findViewById(R.id.TPscoreFacil);
         this.mViewHolder.t_scoreMedio = findViewById(R.id.TPscoreMedio);
         this.mViewHolder.t_scoreDificil = findViewById(R.id.TPscoreDificil);
+        this.mViewHolder.view = findViewById(R.id.TPview);
 
         mudarFonte();
+        mudarPosicao_Tamanho();
 
         // pegar o highscore de cada dificuldade do usuario
         highscoreFacil = 0;
@@ -67,6 +70,7 @@ public class TelaPontucao extends AppCompatActivity {
         TextView t_scoreFacil;
         TextView t_scoreMedio;
         TextView t_scoreDificil;
+        View view;
     }
 
     private void mudarFonte()
@@ -76,5 +80,47 @@ public class TelaPontucao extends AppCompatActivity {
         mViewHolder.t_scoreMedio.setTypeface(fonte);
         mViewHolder.t_scoreDificil.setTypeface(fonte);
         mViewHolder.b_apagar.setTypeface(fonte);
+    }
+
+    private void mudarPosicao_Tamanho()
+    {
+        mViewHolder.view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                int alturaObjeto;
+                int tamanho;
+
+                //Remove o listenner para não ser novamente chamado.
+                mViewHolder.view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                //Coloca a largura igual à altura
+                larguraTela = mViewHolder.view.getWidth();
+                alturaTela = mViewHolder.view.getHeight();
+
+                alturaObjeto = mViewHolder.b_voltar.getHeight();
+                tamanho = mudarTamanho(alturaTela,alturaObjeto);
+                mViewHolder.b_voltar.getLayoutParams().height = tamanho;
+                mViewHolder.b_voltar.requestLayout();
+
+                alturaObjeto = mViewHolder.b_apagar.getHeight();
+                tamanho = mudarTamanho(alturaTela,alturaObjeto);
+                mViewHolder.b_apagar.getLayoutParams().height = tamanho;
+                mViewHolder.b_apagar.requestLayout();
+
+                alturaObjeto = (int) mViewHolder.t_scoreDificil.getTextSize();
+                tamanho = mudarTamanho(alturaTela,alturaObjeto)/3;
+                mViewHolder.t_scoreFacil.setTextSize(tamanho);
+                mViewHolder.t_scoreMedio.setTextSize(tamanho);
+                mViewHolder.t_scoreDificil.setTextSize(tamanho);
+
+            }
+        });
+    }
+
+    private int mudarTamanho(int tamanhoTela, int tamanhoObjeto)
+    {
+        int tamanho;
+        tamanho = tamanhoTela * tamanhoObjeto / 1920;
+        return tamanho;
     }
 }
